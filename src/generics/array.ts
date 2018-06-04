24d59a8e65cfd34d6d5ec2a0d558704091ad9340
Denis Reshetniak <denis_reshetniak@epam.com>
2018-06-04 09:52:26 +0200

    Array example

---
+++ src/generics/array.ts
@@ -0,0 +1,16 @@
+
+interface MyArray<T> {
+  push(...items: T[]): number;
+}
+
+// step1: more descriptive name
+interface MyArray1<Type> {
+  push(...items: Type[]): number;
+}
+
+let arr: MyArray<string>;
+
+// step2: Use provided type instead of variable
+interface MyArray2 {
+  push(...items: string[]): number;
+}
